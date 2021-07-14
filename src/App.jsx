a642ff15a3b2bf60778c3ac55ad3c936afc4dc8c
shantanu-gupta9617 <shantanu9617@gmail.com>
2021-07-14 19:12:59 +0530

    Added Header ,Layout

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Route } from "react-router-dom";
+
+// HOC
+import DefaultHOC from "./HOC/Default.HOC";
+
+// Components
+import Temp from "./components/temp";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <>
+      <DefaultHOC path="/" exact component={Temp} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/planning.js
@@ -0,0 +1,21 @@
+//Book my show planning
+
+//->We need a reusable carousal
+
+//->Hero carousal
+//->common caurosal
+//->cast and crew samll carousal
+
+
+//Pages
+//->Home Page
+//->Movie Page
+//->Categories Page
+//->Auth Page
+//->Razorpay payment gateway
+
+//Plan->How are we building it???? 🤔
+//Layouts
+//Routes
+//Pages
+//Components
